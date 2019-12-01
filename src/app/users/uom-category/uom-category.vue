fa803649be2eb38987e5c6dd7796b0e5285880e3
Samira Saad Ragheb AbdElShaheed <Samira.Ragheb@andalusia.loc>
2019-12-01 11:17:57 +0200

    uom list styles

--- src/app/users/uom-category/uom-category.vue
+++ src/app/users/uom-category/uom-category.vue
@@ -41,7 +41,7 @@
                             :filterable-cell-template="statusFilter"
                             :filterable-cell-show-operators="false"
                             :editor="customBoolEditor"
-                            :template="customTemp"
+                            :template="`#= Status ? '<div class= bg-active-status> Active </div>' : '<div class= bg-inactive-status> Inactive </div> '#`"
                             :width="100"></kendo-grid-column>
             <kendo-grid-column :command="customGridCommand"
                             :title= "this.$i18n.t('Users.Action')"
