59d1afca6dd07563045de15a3a635e39c3c4dfda
Samira Saad Ragheb AbdElShaheed <Samira.Ragheb@andalusia.loc>
2019-12-01 16:57:34 +0200

    shared grid styles

--- src/app/inventory/setups/product-setup/uom-category/uom-category.js
+++ src/app/inventory/setups/product-setup/uom-category/uom-category.js
@@ -1,17 +1,243 @@
+import json from '../../../../../../public/mock-data/uom-category.json'
+
+import $ from 'jquery';
+
+localStorage.getItem('selectedLang') == 'en' ? import('../../../theme/inventory.scss') : import('../../../theme/inventory-rtl.scss');
+
+//Module localization 
+ import localeEn from '../../../locales/en'
+ import localeAr from '../../../locales/ar'
+
 export default {
-    name: "uom-category",
-    data () {
+    name: 'UomCategory',
+    data() {
         return {
+            checked: true,    
+            customGridCommand: [
+                {
+                    name: 'edit',
+                    text: { edit: "Edit", cancel: "Cancel", update: "Update" },
+                    iconClass: {
+                        edit: "fas fa-edit",
+                        update: "fas fa-check-circle",
+                        cancel: "far fa-times-circle"
+                    }
+                },
+                {
+                    name: 'destroy',
+                    text: 'Delete',
+                    iconClass: 'fas fa-trash-alt'
+                }
+            ],
+            productsDataSource: {
+                data: json,
+                schema: {
+                    model: {
+                        id: "ProductID",
+                        fields: {
+                        ProductID: { editable: false, nullable: true },
+                        Code: {  validation: { min: 1 } },
+                        EnName: { validation: { required: {message: this.$i18n.t('Common.Required')} } },
+                        ArName: {  validation: { required: true } },
+                        ProductClassification: {  },
+                        Description: {  },
+                        Status: { type: 'boolean' } 
+                        }
+                    }
+                },
+                pageSize: 10
+            }
         }
     },
     methods: {
+        customBoolEditor(container, options) {
+            $('<input type="checkbox" name="' + options.field + '"/>')
+                  .appendTo(container)
+                  .kendoMobileSwitch({
+                  onLabel: "YES",
+                  offLabel: "NO"
+            });
+          },
+        preventEditColumn(e) { 
+        console.log(e.isNew());
+        if (e.isNew()) {
+            return true;
+        } else {
+            return false
+        }
+        },
+        // DropdownList Filter in Grid
+        statusFilter(args) { 
+            args.element.kendoDropDownList({
+                dataSource: {
+                    data: [
+                    { text: this.$i18n.t('Common.Active'), value: true },
+                    { text: this.$i18n.t('Common.InActive'), value: false }
+                    ]
+                },
+                dataTextField: "text",
+                dataValueField: "value",
+                valuePrimitive: true,
+                optionLabel: "All",
+                autoWidth: true
+            });
+        },
+        detailInit: function (e) {
+            $('<div />').appendTo(e.detailCell).kendoGrid({
+              dataSource: {
+                data: json,
+                schema: {
+                    model: {
+                        fields: {
+                            // 'UOM[dataItem]UOMID': {},
+                            'UOM.UOMID': {},
+                            UOMCode: { type: 'number', validation: { min: 1 } },
+                            UOMEnName: { validation: { required: { message: this.$i18n.t('Common.Required') } } },
+                            UOMArName: {  validation: { required: { message: this.$i18n.t('Common.Required') } } },
+                            UOMDescription: { type: 'string' },
+                            UOMStatus: { type: 'boolean' } 
+                        }
+                    }
+                },
+                // pageSize: 10,
+                filter: {
+                  field: 'ProductID',
+                  operator: 'contains',
+                  value: e.data.ProductID
+
+                },
+                batch: 'true'
+              },
+              toolbar: [
+                  {
+                    name: 'create',
+                    text: this.$i18n.t('Inventory.AddUOM')
+                  }
+              ],
+              filterable:{
+                  mode:'row',
+                  extra:false, 
+                  //cellOperator:'contains'
+                },
+              editable: 'inline',
+              scrollable: true,
+              sortable: true,
+              noRecords: true,
+              columns: [{
+                field: 'UOM.UOMCode',
+                title: this.$i18n.t('Inventory.UOMCode'),
+                // editable: this.preventEditColumn,
+                width: '120px',
+                filterable: {
+                    cell: {
+                        showOperators: false,
+                        operator: 'contains'
+                    }
+                }
+            //   filterable:{
+            // cell:false},
+
+              },
+              {
+                field: 'UOM.UOMEnName',
+                title: this.$i18n.t('Inventory.UOMEnName'),
+                width: '180px',
+                filterable: {
+                    cell: {
+                        showOperators: false,
+                        operator: 'contains'
+                    }
+                }
+              },
+              {
+                field: 'UOM.UOMArName',
+                title: this.$i18n.t('Inventory.UOMArName'),
+                width: '180px',
+                filterable: {
+                    cell: {
+                        showOperators: false,
+                        operator: 'contains'
+                    }
+                }
+              },
+              {
+                field: 'UOM.UOMDescription',
+                title: this.$i18n.t('Inventory.Description'),
+                width: '220px',
+                filterable: {
+                    cell: {
+                        showOperators: false,
+                        operator: 'contains'
+                    }
+                }
+              },
+              {
+                field: 'UOM.UOMReference',
+                title: this.$i18n.t('Inventory.Reference'),
+                width: '170px',
+                filterable: {
+                    cell: {
+                        showOperators: false,
+                        operator: 'contains'
+                    }
+                }
+              },
+              {
+                field: 'UOM.UOMRatio',
+                title: this.$i18n.t('Inventory.Ratio'),
+                width: '140px',
+                filterable: {
+                    cell: {
+                        showOperators: false,
+                        operator: 'contains'
+                    }
+                }
+              },
+            //   {
+            //     field: 'UOM.UOMStatus',
+            //     title: this.$i18n.t('Inventory.Status'),
+            //     editor: this.customBoolEditor,
+            //     width: '130px',
+            //     template: "#= UOM.UOMStatus ? '<div class= bg-active-status> Active </div>' : '<div class= bg-inactive-status> Inactive </div> '#",
+            //     filterable: {
+            //         cell: {
+            //             showOperators: false,
+            //             operator: 'contains'
+            //         }
+            //     }
+            //   },
+              {
+                command: [
+                    {
+                        name: 'edit',
+                        text: { edit: "Edit", cancel: "Cancel", update: "Update" },
+                        iconClass: {
+                            edit: "fas fa-edit",
+                            update: "fas fa-check-circle",
+                            cancel: "far fa-times-circle"
+                        }
+                    },
+                    {
+                        name: 'destroy',
+                        text: 'Delete',
+                        iconClass: 'fas fa-trash-alt'
+                    }
+                ],
+                title: this.$i18n.t('Inventory.Action'),
+                width: '200x'
+              }
+              ]
+            })
+        }
     },
-    components: {
-      
-    },
-    created() {
-    //   this.$store.dispatch(actionsTypes.FETCH_USER_DATA);
-    },
-    mounted() {
+    i18n: {
+        messages: {
+            en: {
+                ...localeEn
+            },
+            ar: {
+                ...localeAr
+            }
+        },
     }
-  };
+}
--- src/app/inventory/setups/product-setup/uom-category/uom-category.vue
+++ src/app/inventory/setups/product-setup/uom-category/uom-category.vue
@@ -1,7 +1,57 @@
 <template>
     <div class="uom-category">
-        uom-category created
+        <kendo-grid :data-source="productsDataSource"
+                :sortable="true"
+                :no-records="true"
+                :navigatable='true'
+                :editable="'inline'"
+                :toolbar="[{name: 'create', text: this.$i18n.t('Inventory.NewCategory')}]"
+                :filterable-extra="false"
+                :filterable-mode="'row'"
+                :pageable-always-visible="true"
+                @detailinit="detailInit" 
+                :filterable-cell-operator="'contains'"
+                :pageable-page-sizes="[5, 10, 20, 30]">
+            <kendo-grid-column :field="'Code'"
+                            :title= "this.$i18n.t('Inventory.CategoryCode')"
+                            :editable="preventEditColumn"
+                            :filterable-cell-show-operators="false"
+                            :filterable-cell-operator="'contains'"
+                            :width="100"></kendo-grid-column>
+            <kendo-grid-column :field="'EnName'"
+                            :title= "this.$i18n.t('Inventory.CategoryEnName')"
+                            :filterable-cell-show-operators="false"
+                            :filterable-cell-operator="'contains'"
+                            :width="120"></kendo-grid-column>
+            <kendo-grid-column :field="'ArName'"
+                            :title= "this.$i18n.t('Inventory.CategryArName')"
+                            :filterable-cell-show-operators="false"
+                            :filterable-cell-operator="'contains'"
+                            :width="120"></kendo-grid-column>
+           
+            <kendo-grid-column :field="'Description'"
+                            :title= "this.$i18n.t('Inventory.Description')"
+                            :filterable-cell-show-operators="false"
+                            :filterable-cell-operator="'contains'"
+                            :width="120"
+                            ></kendo-grid-column> 
+            
+            <kendo-grid-column :field="'Status'"
+                            :title= "this.$i18n.t('Inventory.Status')"
+                            :filterable-cell-template="statusFilter"
+                            :filterable-cell-show-operators="false"
+                            :editor="customBoolEditor"
+                            :template="`#= Status ? '<div class= bg-active-status> Active </div>' : '<div class= bg-inactive-status> Inactive </div> '#`"
+                            :width="100"></kendo-grid-column>
+            <kendo-grid-column :command="customGridCommand"
+                            :title= "this.$i18n.t('Inventory.Action')"
+                            :width="100"></kendo-grid-column>
+        </kendo-grid>
     </div>
 </template>
-        
+
 <script src="./uom-category.js"></script>
+
+<style lang="scss" scoped>
+@import '../../../theme/inventory.scss';
+</style>
